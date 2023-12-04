d5428b6e88c5f289b862bc6e113968efce3d5729
oleksand.stepurko <stepurko.alexandr@gmail.com>
2023-12-04 20:12:42 +0100

    Classwork 2 program structure operations

---
+++ classwork/src/lesson_02/HelloWorld.java
@@ -0,0 +1,11 @@
+package lesson_02;
+
+import java.util.List;
+
+public class HelloWorld {
+
+    public static void main(String[] args) {
+        System.out.println(args[0]);
+        System.out.println(args[1]);
+    }
+}
