71439ad279a6c94e5d8ead557332d1581204d4ee
oleksand.stepurko <stepurko.alexandr@gmail.com>
2023-12-21 20:09:35 +0100

    Classwork 7 diamond problem hashCode/equals enum

---
+++ classwork/src/com/hillel/lesson_07/EnumTest.java
@@ -0,0 +1,11 @@
+package com.hillel.lesson_07;
+
+import com.hillel.lesson_07.enumer.SearchSystem;
+
+public class EnumTest {
+
+    public static void main(String[] args) {
+        System.out.println(SearchSystem.GOOGLE.url());
+    }
+
+}
